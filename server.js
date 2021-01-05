f37d3cac700b26bd27e2bc7525251d6a8382f1c2
Kartheek Palla <palla.kartheekreddy@gmail.com>
2021-01-05 20:05:39 +0530

    math plugin issue fix

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     proxy = require('express-http-proxy'),
     urlHelper = require('url');
-
+const latexService = require('./latexService.js')
 
 var app = express();
 
 app.set('port', 3000);
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ limit: '50mb' }))
+
+app.get("/latex/convert", latexService.convert)
+app.post("/latex/convert", bodyParser.json({ limit: '1mb' }), latexService.convert);
+
 
 app.use('/api', proxy('dock.sunbirded.org', {
     https: true,
@@ -22,9 +26,11 @@ app.use('/api', proxy('dock.sunbirded.org', {
         // you can update headers 
         proxyReqOpts.headers['Content-Type'] = 'application/json';
         proxyReqOpts.headers['user-id'] = 'content-editor';
-        proxyReqOpts.headers['authorization'] = 'Bearer API_KEY';
+        proxyReqOpts.headers['authorization'] = 'Bearer ';
         return proxyReqOpts;
     }
 }));
+
+
 
 http.createServer(app).listen(app.get('port'), 3000);
